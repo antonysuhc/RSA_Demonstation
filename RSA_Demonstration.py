0f747e5151f3ce0b0d6c1c85778b46f6f205ac04
Antony Su <antonysuhc@gmail.com>
2022-07-30 19:08:23 -0700

    Sometimes works. Don't know why.

--- RSA_Demonstration.py
+++ RSA_Demonstration.py
@@ -2,7 +2,7 @@
 import random
 
 def generatePrimes(a, b, n): #generates n prime numbers between a and b.
-    return 7, 13 #write an actual thing
+    return 127, 131  #write an actual thing
 
 def lcm(a, b): #returns least common multiple of a and b.
     return int(a/(gcd(a, b)) * b)
@@ -15,14 +15,33 @@ def gcd(a, b): #returns the greateast common divisor of a and b. Euclidean algor
         a = remainder
     return b
     
-def isDivisor(a, b): #returns TRUE if a is a divisor of b. 
-    return (b%a == 0)
+def isCoPrime(a, b): #returns TRUE if a is a coprime of b. 
+    return (gcd(a, b) != 1)
 
-def modularMultiplicativeInverse(a, b): #returns d, given d*e = 1 mod (b)
+def modularMultiplicativeInverse(a, b): #returns d, given d*a = 1 mod (b). !Requires b to be prime. Euler's Theorem.
     if(gcd(a, b) == 1): #Check if a and b are coprime.
-        return (a % b + b) % b
+        return (a**(b-2) % b)
     else:
-        print("oiwiefohfowieh")
+        raise Exception("Multiplicative Modulo Inverse does not exist for " + str(a) + str(b))
 
 
-print(modularMultiplicativeInverse(5, 13))
+
+if __name__ == "__main__":
+    p, q = generatePrimes(2, 3, 1)
+    n = p*q
+    phiN = lcm(p-1, q-1)
+    print("phiN is " + str(phiN))   
+    e = 109   #Write generate e function
+    if isCoPrime(e, phiN):
+        raise Exception("e is coprime with phiN")
+    d = modularMultiplicativeInverse(e, phiN)
+    print("Public Key is: " + str(e) + ", " +str(n))
+    print("Private Key is: " + str(d) + ", " + str(n))
+    plainText = int(input("Input plaintext: "))
+    cipherText = plainText**e % n
+    print("Ciphertext is: " + str(cipherText))
+    key = int(input("Input key: "))
+    message = cipherText**key % n
+    print(message)
+
+
